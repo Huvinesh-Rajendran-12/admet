bb5be0cc8b12c421d4cecd67f75162212c738e0a
Huvinesh Rajendran <huvinesh@teleme.co>
2025-02-23 17:40:24 +0800

    changed the set_seed() fn

--- utils.py
+++ utils.py
@@ -5,8 +5,11 @@ def set_device() -> torch.device:
     return device
 
 def set_seed(seed: int = 42) -> None:
+    print(f"Seed: {seed}, CUDA available: {torch.cuda.is_available()}, MPS available: {torch.mps.is_available()}")
+    print(f"CUDA device count: {torch.cuda.device_count()}, Current device: {torch.cuda.current_device()}")
     torch.manual_seed(seed)
     if torch.cuda.is_available():
-        torch.cuda.manual_seed(seed)
-    if torch.mps.is_available():
+        torch.cuda.init()
+        torch.cuda.manual_seed_all(seed)
+    elif torch.mps.is_available():
         torch.mps.manual_seed(seed)
